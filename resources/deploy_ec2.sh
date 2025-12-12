365c4610b196f18ac53f9e2528687cba026118d9
Daniel Naranjo <23096864@saultcollege.ca>
2025-12-12 04:02:27 +0000

    changes in ec2.yml to fix test

--- resources/deploy_ec2.sh
+++ resources/deploy_ec2.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# update keys and restart the EC2-hosted dice app
-# update ec2 intance and it is storing in dynamodb
 
 # This gets run ON the EC2 instance (NOT in the GitHub Actions runner)
 
